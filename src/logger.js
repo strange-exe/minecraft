fbeac344bf3b4f81f57c0c6354ac1e5aca680dfd
Abhinesh <work.abhinesh@gmail.com>
2025-01-03 02:42:20 +0530

    Delete src directory

--- src/logger.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function logMessage(message) {
-    const timestamp = new Date().toISOString();
-    fs.appendFileSync('chat_log.txt', `[${timestamp}] ${message}\n`);
-    console.log(message);
-}
-
-module.exports = { logMessage };
